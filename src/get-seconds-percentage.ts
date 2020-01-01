8f8d38bd4d648919c82b9a13c08e75eeb39c5082
Abdullah <abdullah@tayel.com>
2020-01-01 21:00:53 +0200

    fix conflict

--- src/get-seconds-percentage.ts
+++ src/get-seconds-percentage.ts
@@ -6,10 +6,12 @@ function getSecondsPercentage(date: Date) {
   const currentHours = date.getHours() + currentMinutes / 60;
   const currentDay = date.getDay() + currentHours / 24;
 
+  const dayPercentage = currentHours / 24;
   const hourPercentage = currentHours % 1;
   const minutePercentage = currentMinutes % 1;
   const secondPercentage = currentSeconds % 1;
-  return { hourPercentage, minutePercentage, secondPercentage };
+  return { hourPercentage, minutePercentage, dayPercentage };
 }
 
 export { getSecondsPercentage };
+
